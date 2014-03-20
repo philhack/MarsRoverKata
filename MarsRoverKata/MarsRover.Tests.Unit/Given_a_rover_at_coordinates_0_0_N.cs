07ccefc968fce803b08fcb8bc773f321f41c4092
Phil Hack <phil@philhack.com>
2014-03-20 07:46:13 +0000

    implemented failing test for executing the Backwards command

--- MarsRoverKata/MarsRover.Tests.Unit/Given_a_rover_at_coordinates_0_0_N.cs
+++ MarsRoverKata/MarsRover.Tests.Unit/Given_a_rover_at_coordinates_0_0_N.cs
@@ -28,4 +28,18 @@ namespace Given_a_rover_at_coordinates_0_0_N {
             Assert.AreEqual(expected, Rover.CurrentLocation());
         }
     }
+
+    public class When_the_command_B_is_given : Given_a_rover_at_coordinates_0_0_N {
+    [SetUp]
+        public void When() {
+            Rover.ExecuteCommands("B");
+        }
+
+        [Test]
+        public void Then_the_robots_position_is_now_at_0_0_N() {
+            var expected = "0,0,N";
+
+            Assert.AreEqual(expected, Rover.CurrentLocation());
+        }
+    }
 }
