54613f389f2786750e0de0d7d42c5ff4a3139669
Phil Hack <phil@philhack.com>
2014-03-20 07:55:09 +0000

    implemented failing test to move rover Forward Forward

--- MarsRoverKata/MarsRover.Tests.Unit/Given_a_rover_at_coordinates_0_0_N.cs
+++ MarsRoverKata/MarsRover.Tests.Unit/Given_a_rover_at_coordinates_0_0_N.cs
@@ -29,8 +29,22 @@ namespace Given_a_rover_at_coordinates_0_0_N {
         }
     }
 
+    public class When_the_command_FF_is_given : Given_a_rover_at_coordinates_0_0_N {
+        [SetUp]
+        public void When() {
+            Rover.ExecuteCommands("FF");
+        }
+
+        [Test]
+        public void Then_the_robots_position_is_now_at_0_2_N() {
+            var expected = "0,2,N";
+
+            Assert.AreEqual(expected, Rover.CurrentLocation());
+        }
+    }
+
     public class When_the_command_B_is_given : Given_a_rover_at_coordinates_0_0_N {
-    [SetUp]
+        [SetUp]
         public void When() {
             Rover.ExecuteCommands("B");
         }
