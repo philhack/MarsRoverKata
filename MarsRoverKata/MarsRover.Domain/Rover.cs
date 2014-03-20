ff55b26815a345e00e80820224766123b9ff37ac
Phil Hack <phil@philhack.com>
2014-03-20 07:42:00 +0000

    first failing test to move rover forward

---
+++ MarsRoverKata/MarsRover.Domain/Rover.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MarsRover.Domain {
+    public class Rover {
+        public void ExecuteCommands(string commands) {
+            throw new NotImplementedException();
+        }
+
+        public string CurrentLocation() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarsRoverKata/MarsRover.Tests.Unit/Given_a_rover_at_coordinates_0_0_N.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsRover.Domain;
+using NUnit.Framework;
+
+namespace Given_a_rover_at_coordinates_0_0_N {
+    public class Given_a_rover_at_coordinates_0_0_N {
+        protected Rover Rover;
+
+        [SetUp]
+        public void Given() {
+            Rover = new Rover();
+        }
+    }
+
+    public class When_the_command_F_is_given : Given_a_rover_at_coordinates_0_0_N {
+        [SetUp]
+        public void When() {
+            Rover.ExecuteCommands("F");
+        }
+
+        [Test]
+        public void Then_the_robots_position_is_now_at_1_0_N() {
+            var expected = "1,0,N";
+
+            Assert.AreEqual(expected, Rover.CurrentLocation());
+        }
+    }
+}
