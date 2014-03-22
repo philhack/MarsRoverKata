f69ee3406ea04c640e6f20272bb249eba1a1a4a0
Phil Hack <phil@philhack.com>
2014-03-22 22:40:29 +0000

    updated acceptance test

--- MarsRoverKata/MarsRover.Tests.Acceptance/MoveRoverSteps.cs
+++ MarsRoverKata/MarsRover.Tests.Acceptance/MoveRoverSteps.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using MarsRover.Domain;
+using MarsRover.Domain.Interfaces;
+using NUnit.Framework;
 using TechTalk.SpecFlow;
 
 namespace MarsRover.Tests.Acceptance {
     [Binding]
     public class MoveRoverSteps {
+        private IRover _rover;
+        IRoverInvoker _roverInvoker;
+        IRoverClient _roverClient;
+
         [Given(@"The rover is located at ""(.*)""")]
         public void GivenTheRoverIsLocatedAt(string p0) {
-            ScenarioContext.Current.Pending();
+            _rover = new Rover();
+            _roverInvoker = new RoverInvoker();
+            _roverClient = new RoverClient(_rover, _roverInvoker);
+
+            StringAssert.Contains(p0, _rover.CurrentLocation());
         }
 
         [Given(@"is on a ""(.*)"" grid")]
@@ -16,12 +27,12 @@ namespace MarsRover.Tests.Acceptance {
 
         [When(@"the rover is given the command ""(.*)""")]
         public void WhenTheRoverIsGivenTheCommand(string p0) {
-            ScenarioContext.Current.Pending();
+            _roverClient.GiveCommands(p0);
         }
 
         [Then(@"the rover is at ""(.*)""\.")]
         public void ThenTheRoverIsAt_(string p0) {
-            ScenarioContext.Current.Pending();
+            StringAssert.Contains(p0, _rover.CurrentLocation());
         }
     }
 }
