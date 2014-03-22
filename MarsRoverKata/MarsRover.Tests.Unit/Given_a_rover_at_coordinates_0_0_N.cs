b16b39580500722308e55e0ebcb9763ff4e9c277
Phil Hack <phil@philhack.com>
2014-03-22 13:04:51 +0000

    corrected grammar and conformed to a domain specific language

--- MarsRoverKata/MarsRover.Tests.Unit/Given_a_rover_at_coordinates_0_0_N.cs
+++ MarsRoverKata/MarsRover.Tests.Unit/Given_a_rover_at_coordinates_0_0_N.cs
@@ -31,21 +31,21 @@ namespace Given_a_rover_at_coordinates_0_0_N {
         }
 
         [Test]
-        public void Then_the_robots_position_is_now_at_0_1_N() {
+        public void Then_the_rovers_current_locatyion_is_now_at_0_1_N() {
             var expected = "0,1,N";
 
             Assert.AreEqual(expected, Rover.CurrentLocation());
         }
     }
 
-    public class When_the_command_FF_is_given : Given_a_rover_at_coordinates_0_0_N {
+    public class When_the_commands_FF_are_given : Given_a_rover_at_coordinates_0_0_N {
         [SetUp]
         public void When() {
             Rover.ExecuteCommands("FF");
         }
 
         [Test]
-        public void Then_the_robots_position_is_now_at_0_2_N() {
+        public void Then_the_rovers_current_locatyion_is_now_at_0_2_N() {
             var expected = "0,2,N";
 
             Assert.AreEqual(expected, Rover.CurrentLocation());
@@ -59,21 +59,21 @@ namespace Given_a_rover_at_coordinates_0_0_N {
         }
 
         [Test]
-        public void Then_the_robots_position_is_now_at_0_0_N() {
+        public void Then_the_rovers_current_locatyion_is_now_at_0_0_N() {
             var expected = "0,0,N";
 
             Assert.AreEqual(expected, Rover.CurrentLocation());
         }
     }
 
-    public class When_the_command_BB_is_given : Given_a_rover_at_coordinates_0_0_N {
+    public class When_the_commands_BB_are_given : Given_a_rover_at_coordinates_0_0_N {
         [SetUp]
         public void When() {
             Rover.ExecuteCommands("BB");
         }
 
         [Test]
-        public void Then_the_robots_position_is_now_at_0_0_N() {
+        public void Then_the_rovers_current_locatyion_is_now_at_0_0_N() {
             var expected = "0,0,N";
 
             Assert.AreEqual(expected, Rover.CurrentLocation());
@@ -87,7 +87,7 @@ namespace Given_a_rover_at_coordinates_0_0_N {
         }
 
         [Test]
-        public void Then_the_robots_position_is_now_at_0_1_N() {
+        public void Then_the_rovers_current_locatyion_is_now_at_0_1_N() {
             var expected = "0,1,N";
             Assert.AreEqual(expected, Rover.CurrentLocation());
         }
