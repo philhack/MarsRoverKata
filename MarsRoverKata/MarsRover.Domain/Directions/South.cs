be70cde0bc84118892e72a4f8033d97a137da049
Phil Hack <phil@philhack.com>
2014-03-23 08:53:04 +0000

    made test pass for Pointing North and Given the command RRR

--- MarsRoverKata/MarsRover.Domain/Directions/South.cs
+++ MarsRoverKata/MarsRover.Domain/Directions/South.cs
@@ -3,7 +3,7 @@
 namespace MarsRover.Domain.Directions {
     public class South : IDirection {
         public IDirection TurnRight() {
-            throw new System.NotImplementedException();
+            return new West();
         }
 
         public IDirection TurnLeft() {
