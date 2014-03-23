7c027a19dabcf45b5c7b51ca8f8d80c96cdc8413
Phil Hack <phil@philhack.com>
2014-03-23 08:56:55 +0000

    added failing test for pointing North and receiving the commands Left Left

--- MarsRoverKata/MarsRover.Tests.Unit/BDD/Given_a_rover_at_coordinates_0_0_N.cs
+++ MarsRoverKata/MarsRover.Tests.Unit/BDD/Given_a_rover_at_coordinates_0_0_N.cs
@@ -193,4 +193,19 @@ namespace Given_a_rover_at_coordinates_0_0_N {
             Assert.AreEqual(expected, Rover.CurrentLocation());
         }
     }
+
+    public class When_the_command_LL_is_given : Given_a_rover_at_coordinates_0_0_N {
+        [SetUp]
+        public void When() {
+            RoverClient.GiveCommands("LL");
+            RoverInvoker.Execute();
+        }
+
+        [Test]
+        public void Then_the_rover_current_location_is_0_0_S() {
+            var expected = "0,0,S";
+
+            Assert.AreEqual(expected, Rover.CurrentLocation());
+        }
+    }
 }
